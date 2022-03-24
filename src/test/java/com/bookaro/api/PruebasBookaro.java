ee4f1962b790a05d577b6dce166c1fa27a2004d7
pEDrOx <pedroxzzz@gmail.com>
2022-03-24 17:39:58 +0100

    Pruebas Junit

--- src/test/java/com/bookaro/api/PruebasBookaro.java
+++ src/test/java/com/bookaro/api/PruebasBookaro.java
@@ -1,9 +1,9 @@
 package com.bookaro.api;
 
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
-import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,39 +12,29 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
+import org.junit.jupiter.api.Test;
 import com.bookaro.api.models.User;
 import com.bookaro.api.repositories.UserRepository;
-
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class BookaroApiApplicationTests {
-
+class PruebasBookaro {
+	
 	@Autowired
 	private UserRepository userRepository;
-	
-	@DynamicPropertySource
-	  static void postgresqlProperties(DynamicPropertyRegistry registry) {
-	    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-	    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-	    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-	  }
-	  @Container
-	  public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11-alpine")
-	      .withDatabaseName("integration-tests-db")
-	      .withPassword("inmemory")
-	      .withUsername("inmemory");
-	
 
 	@Test
-	void contextLoads() {
-		
-		List<User> allCars = (List<User>) userRepository.findAll();
-	    assert !allCars.isEmpty();
-	    assert allCars.get(0).getUsername().equalsIgnoreCase("Pedro");
-		
+	void test() {
+
+		ArrayList<User> allCars = (ArrayList<User>) userRepository.findAll();
+
+		for(User u: allCars) {
+			System.out.println(u.getUsername());
+		}
+		assert !allCars.isEmpty();
+		assert allCars.get(0).getUsername().equalsIgnoreCase("Pedro");
+
 	}
 
 }
